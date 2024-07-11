cf96f56d1ff6d36fcc9b16a0a4df9ef04f13d6ac
Icarogamer2441 <148513434+Icarogamer2441@users.noreply.github.com>
2024-07-11 19:39:37 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,166 @@
+from anim import Animation
+
+# default fps = 12
+an = Animation()
+an.setFps(15)
+
+an.addFrame("""
+        o
+
+
+
+""")
+
+an.addFrame("""
+        o
+
+
+
+""")
+
+an.addFrame("""
+        o
+
+
+
+""")
+
+an.addFrame("""
+         
+        o
+
+
+""")
+
+an.addFrame("""
+         
+        o
+
+
+""")
+
+an.addFrame("""
+         
+
+        o
+
+""")
+
+an.addFrame("""
+         
+
+        o
+
+""")
+
+an.addFrame("""
+         
+
+
+        o
+
+""")
+
+an.addFrame("""
+         
+
+
+
+        o
+""")
+
+an.addFrame("""
+         
+
+
+
+
+        o
+""")
+
+an.addFrame("""
+
+
+
+
+
+
+       ( )
+""")
+
+an.addFrame("""
+
+
+
+
+
+        o
+""")
+
+an.addFrame("""
+
+
+
+
+        o
+
+""")
+
+an.addFrame("""
+
+
+
+        o
+
+
+""")
+
+an.addFrame("""
+
+
+
+
+        o
+
+""")
+
+an.addFrame("""
+
+
+
+
+
+        o
+""")
+
+an.addFrame("""
+
+
+
+
+
+
+       ( )
+""")
+
+an.addFrame("""
+
+
+
+
+
+        o
+
+""")
+
+an.addFrame("""
+
+
+
+
+
+
+        o
+""")
+
+an.start()
